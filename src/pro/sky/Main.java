788d030bba1ded942a76aaf27237531f5b67ea90
sspichuzhkin <103836818+sspichuzhkin@users.noreply.github.com>
2022-05-10 17:09:56 +0300

    hm init (#1)
    * hm init
    
    * hm init
    
    * hm
    refactor

--- src/pro/sky/Main.java
+++ src/pro/sky/Main.java
@@ -2,6 +2,42 @@ package pro.sky;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Task1();
+        Task2();
+        Task3();
+    }
+    public static void Task1() {
+        int i = 1;
+        while (i <= 10) {
+            System.out.print(i + " ");
+            i++;
+        }
+        System.out.println();
+        for (; i > 0; i--) {
+            if (i != 11) {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println();
+    }
+
+    public static void Task2() {
+        int fridayDate = 7;
+        for (int i = fridayDate; i <= 31; i = i + 7) {
+            System.out.println("Сегодня пятница, " + i + "-е число. Необходимо подготовить отчет.");
+        }
+    }
+
+    public static void Task3() {
+        int startYear = 0;
+        int currentYear = 2022;
+        int pastYear = currentYear - 200;
+        int futureYear = currentYear + 100;
+
+        for (int i = startYear; i <= futureYear; i = i + 79) {
+            if (i >= pastYear ) {
+                System.out.println(i);
+            }
+        }
     }
 }
